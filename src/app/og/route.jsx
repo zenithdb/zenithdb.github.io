a252cc3d834257f4a008638e04421bf73a1633c3
Thi Van Le <vannnyle@gmail.com>
2023-07-13 14:49:03 +0200

    fix: remove try catch

--- src/app/og/route.jsx
+++ src/app/og/route.jsx
@@ -23,97 +23,87 @@ export async function GET(request) {
     background,
   ]);
 
-  try {
-    const { searchParams } = request.nextUrl;
-    console.log(request);
+  const { searchParams } = request.nextUrl;
 
-    const hasTitle = searchParams.has('title');
-    const title = hasTitle
-      ? searchParams.get('title')?.slice(0, 100)
-      : 'Serverless, Fault-Tolerant, Branchable Postgres';
+  const title =
+    searchParams.get('title')?.slice(0, 100) || 'Serverless, Fault-Tolerant, Branchable Postgres';
 
-    return new ImageResponse(
-      (
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontFamily: '"IBM Plex Sans"',
+          fontStyle: 'normal',
+          position: 'relative',
+          backgroundColor: '#0A0B0D',
+          backgroundSize: '150px 150px',
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          padding: '44px 56px 56px',
+        }}
+      >
+        <img
+          width="1200"
+          height="630"
+          src={backgroundData}
+          style={{ position: 'absolute', top: 0, left: 0 }}
+        />
+        <img width="229" height="64" src={logoData} />
         <div
           style={{
-            fontFamily: '"IBM Plex Sans"',
-            fontStyle: 'normal',
-            position: 'relative',
-            backgroundColor: '#0A0B0D',
-            backgroundSize: '150px 150px',
-            height: '100%',
-            width: '100%',
             display: 'flex',
             flexDirection: 'column',
-            padding: '44px 56px 56px',
+            marginTop: 'auto',
           }}
         >
-          <img
-            width="1200"
-            height="630"
-            src={backgroundData}
-            style={{ position: 'absolute', top: 0, left: 0 }}
-          />
-          <img width="229" height="64" src={logoData} />
           <div
             style={{
-              display: 'flex',
-              flexDirection: 'column',
-              marginTop: 'auto',
+              fontSize: 100,
+              fontWeight: 500,
+              color: 'white',
+              lineHeight: 1,
+              letterSpacing: '-0.04em',
+              whiteSpace: 'pre-wrap',
+              maxWidth: '90%',
+              marginTop: 10,
             }}
           >
-            <div
-              style={{
-                fontSize: 100,
-                fontWeight: 500,
-                color: 'white',
-                lineHeight: 1,
-                letterSpacing: '-0.04em',
-                whiteSpace: 'pre-wrap',
-                maxWidth: '90%',
-                marginTop: 10,
-              }}
-            >
-              {title}
-            </div>
-            <div
-              style={{
-                fontSize: 30,
-                lineHeight: 1.25,
-                marginTop: 28,
-                letterSpacing: '-0.04em',
-                color: '#C9CBCF',
-                whiteSpace: 'pre-wrap',
-              }}
-            >
-              neon.tech
-            </div>
+            {title}
+          </div>
+          <div
+            style={{
+              fontSize: 30,
+              lineHeight: 1.25,
+              marginTop: 28,
+              letterSpacing: '-0.04em',
+              color: '#C9CBCF',
+              whiteSpace: 'pre-wrap',
+            }}
+          >
+            neon.tech
           </div>
         </div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-        fonts: [
-          {
-            name: 'IBM Plex Sans',
-            data: fontDataMedium,
-            style: 'normal',
-            weight: 500,
-          },
-          {
-            name: 'IBM Plex Sans',
-            data: fontDataNormal,
-            style: 'normal',
-            weight: 400,
-          },
-        ],
-      }
-    );
-  } catch (e) {
-    console.log(`${e.message}`);
-    return new Response(`Failed to generate the image`, {
-      status: 500,
-    });
-  }
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: 'IBM Plex Sans',
+          data: fontDataMedium,
+          style: 'normal',
+          weight: 500,
+        },
+        {
+          name: 'IBM Plex Sans',
+          data: fontDataNormal,
+          style: 'normal',
+          weight: 400,
+        },
+      ],
+    }
+  );
 }
